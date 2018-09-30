e10d9273c15af3c296a878af7779fe5a09d263ca
organaizer12 <organaizer12@ya.ru>
2018-09-30 18:29:54 +0300

    Update openRep.spec.js

--- specs/openRep.spec.js
+++ specs/openRep.spec.js
@@ -3,7 +3,6 @@ var SearchResultPage = require('../pageobjects/searchResult.page');
 var expect = require('chai').expect;
 var Page = require('../pageobjects/page')
 
-
 describe('open home page', function() {
 
 	it('Simple test', function() {
